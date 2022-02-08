0f5e4b0c3ec0e08547b7b007728f2a5768566bad
Noah <gamerpenguin2005@gmail.com>
2022-02-07 21:24:34 -0800

    uhh

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,12 @@ let parent = document.getElementById("parent");
 var elWidth = 64;
 var width = Math.round(window.innerWidth / elWidth) + 2;
 
+var grasses = [
+  "../assets/grass01.png",
+  "../assets/grass02.png",
+  "../assets/grass03.png"
+]
+
 class segment {
   constructor(width, number, parent, index) {
     this.width = width;
@@ -13,6 +19,9 @@ class segment {
     this.el.style.width = this.width + "px";
     this.el.style.transform = `translateX(${this.x}px`;
     this.parent.appendChild(this.el);
+    this.flip = false
+    var bg = grasses[Math.floor(Math.random() * grasses.length)]
+    this.el.style.backgroundImage = `url("${bg}")`
     var _self = this;
     function update() {
       _self.x-= 1;
@@ -20,7 +29,7 @@ class segment {
         _self.x += _self.number * _self.width;
       }
 
-      _self.el.style.transform = `translateX(${_self.x}px`;
+      _self.el.style.transform = `translateX(${_self.x}px)`;
       requestAnimationFrame(update);
     }
     requestAnimationFrame(update);
@@ -78,19 +87,25 @@ setInterval(() => {
 
 let clouds = {
   1: [
-    "./assets/1cloud01.png"
+    "./assets/1cloud01.png",
+    "./assets/1cloud02.png",
+    "./assets/1cloud03.png",
   ],
   2: [
-    "./assets/2cloud01.png"
+    "./assets/2cloud01.png",
+    "./assets/2cloud02.png",
+    "./assets/2cloud03.png",
   ],
   3: [
-    "./assets/3cloud01.png"
+    "./assets/3cloud01.png",
+    "./assets/3cloud02.png",
+    "./assets/3cloud03.png",
   ]
 }
 
 class cloud {
   constructor(speed, z, startPos) {
-    this.startPos = (startPos != null) ? startPos : window.innerWidth
+    this.startPos = (startPos != null) ? (Math.ceil(startPos / 8) * 8) : (Math.ceil(window.innerWidth/ 8) * 8)
     this.speed = speed
     this.width = 512
     this.el = document.createElement("img");
@@ -101,6 +116,8 @@ class cloud {
     this.x = this.startPos;
     var _self = this;
     this.el.style.zIndex  = 11 - z
+
+    if(Math.random() > .5) this.el.classList.add("flipHor")
 
     function update() {
       _self.x -= _self.speed;
@@ -118,7 +135,7 @@ class cloud {
 
 setInterval(() => {
   if(Math.random() > .95) {
-    let speeds = [1, 2, 2, 3, 3, 3]
+    let speeds = [1, 1, 2, 2, 3, 3, 3]
     let speed = speeds[Math.floor(Math.random() * speeds.length)]
     new cloud(1 - ((speed - 1) * .25), speed)
   }
@@ -133,3 +150,107 @@ function genStartClouds() {
   }
 }
 genStartClouds()
+
+
+var bee = document.getElementById("bee")
+var beeY = 0
+var indx = 0
+var beeAnimating = true
+var particleIndex = 0
+var particleStyleIndex = 0
+var particleStyles = [1, 1, 0, 0, 0]
+var offset = Math.floor(Math.random() * 100)
+function animateBee () {
+
+
+  if(beeAnimating) {
+    indx ++ 
+    particleIndex += 2
+    beeY = (Math.sin((indx + offset)/12) * 10) + (Math.sin((indx + offset)/24) * 7)
+    var beeX = (window.innerWidth + 80)  - (indx * 2)
+    if(particleIndex ==8) {
+      var particleStyle = particleStyles[particleStyleIndex]
+      particleStyleIndex ++
+      if(particleStyle == 1){
+        new beeParticle(beeY + (4*8), beeX + 72)
+      }
+      if(particleStyleIndex == particleStyles.length) particleStyleIndex -= particleStyles.length
+      particleIndex = 0
+    }
+    if(beeX < -80) {
+      indx = 0
+      beeAnimating = false
+      beeX = (window.innerWidth + 80)
+    }
+    bee.style.transform = "translate(" + beeX+ "px," + beeY + "px)"
+  } else {
+    if(Math.random() > 0.998) {Math.floor(Math.random() * 100);beeAnimating= true};
+  }
+  var raf = requestAnimationFrame(animateBee)
+}
+
+animateBee()
+
+var beeParticleContainer = document.getElementById("beeParticles")
+
+class beeParticle {
+  constructor (height, x) {
+    this.el = document.createElement('div')
+    this.el.classList.add("beeParticle")
+    this.el.style.left = x + "px"
+    this.el.style.top = height + "px"
+    beeParticleContainer.appendChild(this.el)
+    setTimeout(() => {
+      this.el.parentElement.removeChild(this.el)
+    }, 1000);
+  }
+}
+
+
+var headerDropContent = {
+  "Home": false,
+  "About": {
+    "What is Key Club?": "",
+    "Officers & Advisors": "",
+  },
+  "Services": {
+    "Services": "",
+    "DCM's": "",
+    "District Events": "",
+    "Calendar": "",
+  },
+  "Resources": {
+    "Hours": "",
+    "Document": "",
+    "Cheers": "",
+  },
+  "More": {
+    "Gallery": "",
+    "Announcements": "",
+    "Links": "",
+    "Contact Us": "",
+  },
+}
+var uhb = document.getElementById("underHeaderBanner")
+
+function setupHeader() {
+  document.querySelectorAll("[headerWideBtn]").forEach(element => {
+    element.addEventListener("mouseover", (e) => {
+      if(headerDropContent[e.target.attributes["headerwidebtn"].value]) {
+        uhb.innerHTML = ""
+        uhb.classList.add("bannerOpen")
+        Object.entries(headerDropContent[e.target.attributes["headerwidebtn"].value]).forEach(e => {
+          console.log(e)
+          uhb.innerHTML += `<a class="headerBtn" href="${e[1]}">${e[0]}</a><dot></dot>`
+        })
+        uhb.removeChild(uhb.childNodes[uhb.childNodes.length -1])
+      }
+    })
+    element.addEventListener("mouseout", (e) => {
+      console.log('b')
+      uhb.classList.remove("bannerOpen")
+    })
+  });
+}
+
+setupHeader()
